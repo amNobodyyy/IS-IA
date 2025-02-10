ccffbf901f6ba66638136ca06167bdd7be0d9c31
amNobodyyy <ksuhrud24@gmail.com>
2025-02-10 06:20:17 +0530

    test Outputs

--- packetCapture.py
+++ packetCapture.py
@@ -4,4 +4,4 @@ def packet_callback(packet):
     print(packet.summary())
 
 # Capture 10 packets on the default network interface
-sniff(prn=packet_callback, count=20, iface="Realtek RTL8852BE-VS WiFi 6 802.11ax PCIe Adapter", filter="ip", store=False)
+sniff(prn=packet_callback, count=20, iface="Your_Interface_Name", filter="ip", store=False)
